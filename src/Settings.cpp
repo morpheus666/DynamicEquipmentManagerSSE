2d498e46b508c10353e33be33476a3c1a43e028d
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-07-25 22:06:30 -0700

    Update Settings.cpp

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -3,10 +3,10 @@
 
 bool Settings::loadSettings(bool a_dumpParse)
 {
-	return Json2Settings::Settings::loadSettings(FILE_NAME, false, a_dumpParse);
+	return Json2Settings::Settings::loadSettings(FILE_NAME, a_dumpParse);
 }
 
 
-decltype(Settings::manageAmmo)		Settings::manageAmmo("manageAmmo", true, true);
-decltype(Settings::manageHelmet)	Settings::manageHelmet("manageHelmet", true, true);
-decltype(Settings::manageShield)	Settings::manageShield("manageShield", true, true);
+decltype(Settings::manageAmmo)		Settings::manageAmmo("manageAmmo", true);
+decltype(Settings::manageHelmet)	Settings::manageHelmet("manageHelmet", true);
+decltype(Settings::manageShield)	Settings::manageShield("manageShield", true);
